0109997119d0e264911f41fa77417a8321dbc9fa
Sadnya20 <sadnyakolhe2011@gmail.com>
2024-12-23 11:35:30 +0530

    ssecond  commit

--- java.js
+++ java.js
@@ -7,3 +7,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("Even Numbers:", findEvenNumbers(numbers));
 console.log("Odd Numbers:", findOddNumbers(numbers));
 
+// Finding odd numbers in an array
+function findOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+
