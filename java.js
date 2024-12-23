4bd24b8a86a9d5f7c2014b61545286ab538f7fce
Sadnya20 <sadnyakolhe2011@gmail.com>
2024-12-23 11:33:06 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,9 @@
+// Finding even numbers in an array
+function findEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Even Numbers:", findEvenNumbers(numbers));
+console.log("Odd Numbers:", findOddNumbers(numbers));
+
